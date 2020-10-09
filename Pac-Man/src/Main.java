cf7ade969438daf53138fd7661eae17a0b2b3e89
Alexandros <aaidonis1932@bths.edu>
2020-10-09 13:18:51 -0400

    First Commit

---
+++ Pac-Man/src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class Main {
+	public static void main(String args[])
+	{
+		JFrame frame = new JFrame("Pac-Man");
+		frame.setSize(500,500);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	}
+}

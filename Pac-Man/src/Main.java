8b0daece309d86347906d6dffa08e5f273e6f0ce
TanmoyPaul1 <pault@bxscience.edu>
2020-10-10 19:36:54 -0400

    Fixed label and added name TextField

--- Pac-Man/src/Main.java
+++ Pac-Man/src/Main.java
@@ -1,39 +1,72 @@
 import java.awt.Color;
 import javax.swing.*;
-//import javax.swing.JLabel;
-//import javax.swing.WindowConstants;
+import java.awt.*;
+import java.awt.event.*;
 
-public class Main 
+public class Main implements ActionListener
 {
-	public static void field(JFrame frame)
-	{
+	private static JFrame frame;
+	private static JLabel PacManLabel, nameLabel;
+	private static JTextField nameText;
+	private static JButton startButton;
+
+	public Main(){
+		//Creates the window to play on. 
+		frame = new JFrame("Pac-Man");
 		frame.setSize(700,500);
-		frame.setVisible(true);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.getContentPane().setBackground(Color.BLACK);
 		frame.setLayout(null);
-	}
-	public static void start(JFrame frame)
-	{
-		JButton startButton = new JButton(new ImageIcon("images/start.png"));
+		
+		//Creates the enter name text.
+		nameLabel = new JLabel("Enter your name:");
+		nameLabel.setBounds(263,150,200,50);
+		nameLabel.setFont(new Font("", Font.PLAIN, 20));
+		nameLabel.setForeground(Color.RED);
+		
+		//Creates text bow to enter name in.
+		nameText = new JTextField("");  
+		nameText.setBounds(250,200,200,50);
+		nameText.setVisible(true);
+		nameText.setForeground(Color.RED);
+	    
+		//Creates the start button. 		
+		startButton = new JButton(new ImageIcon("images/start.png"));
 		startButton.setBounds(100,300,500,115);
+		startButton.addActionListener(this);
+		
+		frame.add(nameText);
+		frame.add(nameLabel);
 		frame.add(startButton);
 	}
+	
+    public void actionPerformed(ActionEvent e) {  
+    	String s1 = nameText.getText();
+    	if(e.getSource()==startButton){  
+        	frame.remove(startButton);
+        }
+
+    }
+
+    public static void function()
+	{
+				
+	}
+	
 	public static void main(String args[])
 	{
-		JFrame frame = new JFrame("Pac-Man");
+		new Main();
 		
-		//Creates the window to play on. 
-		field(frame);
+		PacManLabel = new JLabel("Pac-Man");
+		PacManLabel.setBounds(193,20,400,50);
+		PacManLabel.setFont(new Font("", Font.BOLD, 70));
+		PacManLabel.setForeground(Color.YELLOW);
+		frame.add(PacManLabel);
 		
-		//Creates the start button. 
-		start(frame);
-		
-		JLabel label = new JLabel("Pac-Man");
-		label.setBounds(325,10,200,200);
-		label.setVisible(true);
-		label.setForeground(Color.YELLOW);
-		frame.add(label);
+		frame.setLocationRelativeTo(null);
+		frame.revalidate();
+		frame.setVisible(true);
+
 	}
 }
 

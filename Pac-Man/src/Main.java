2a68947a52f4d63888f3795f5f370dca1e8c23dc
TanmoyPaul1 <pault@bxscience.edu>
2020-10-10 00:55:22 -0400

    Added START button

--- Pac-Man/src/Main.java
+++ Pac-Man/src/Main.java
@@ -1,24 +1,39 @@
 import java.awt.Color;
+import javax.swing.*;
+//import javax.swing.JLabel;
+//import javax.swing.WindowConstants;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.WindowConstants;
-
-public class Main {
-	public static void main(String args[])
+public class Main 
+{
+	public static void field(JFrame frame)
 	{
-		JFrame frame = new JFrame("Pac-Man");
-		frame.setSize(500,500);
+		frame.setSize(700,500);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.getContentPane().setBackground(Color.BLACK);
-		JLabel label = new JLabel("Pac-Man");
 		frame.setLayout(null);
-		label.setBounds(100,100,100,100);
+	}
+	public static void start(JFrame frame)
+	{
+		JButton startButton = new JButton(new ImageIcon("images/start.png"));
+		startButton.setBounds(100,300,500,115);
+		frame.add(startButton);
+	}
+	public static void main(String args[])
+	{
+		JFrame frame = new JFrame("Pac-Man");
+		
+		//Creates the window to play on. 
+		field(frame);
+		
+		//Creates the start button. 
+		start(frame);
+		
+		JLabel label = new JLabel("Pac-Man");
+		label.setBounds(325,10,200,200);
 		label.setVisible(true);
 		label.setForeground(Color.YELLOW);
 		frame.add(label);
-		System.out.println("");
 	}
 }
 

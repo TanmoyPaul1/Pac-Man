e417adc07a07d053c797bb8968d2de9d86ae0a43
Alexandros <aaidonis1932@bths.edu>
2021-01-08 12:05:19 -0500

    Solution not working

--- Pac-Man/src/Enemies.java
+++ Pac-Man/src/Enemies.java
@@ -48,18 +48,84 @@ public class Enemies extends Main
 		TimerTask tt = new TimerTask() {  
 		    @Override  
 		    public void run() {  
-		        redX = move(redX); 						if(redX >= 948){ redX = 948;} if(redX <= 10){ redX = 10;} 
-		        redY = move(redY); 						if(redY >= 611){ redY = 611;} if(redY <= 10){ redY = 10;} 
-				red.setBounds(redX, redY, 30, 42);
-				pinkX = move(pinkX); 					if(pinkX >= 948){ pinkX = 948;} if(pinkX <= 10){ pinkX = 10;} 
-				pinkY = move(pinkY);					if(pinkY >= 611){ pinkY = 611;} if(pinkY <= 10){ pinkY = 10;} 
-		        pink.setBounds(pinkX, pinkY, 30, 42);
-		        cyanX = move(cyanX); 					if(cyanX >= 948){ cyanX = 948;} if(cyanX <= 10){ cyanX = 10;} 
-		        cyanY = move(cyanY);					if(cyanY >= 611){ cyanY = 611;} if(cyanY <= 10){ cyanY = 10;} 
-		        cyan.setBounds(cyanX, cyanY, 30, 42);
-		        orangeX = move(orangeX); 		if(orangeX >= 948){ orangeX = 948;} if(orangeX <= 10){ orangeX = 10;} 
-		        orangeY = move(orangeY);		if(orangeY >= 611){ orangeY = 611;} if(orangeY <= 10){ orangeY = 10;} 
-		        orange.setBounds(orangeX, orangeY, 30, 42);
+		    	if((Math.abs(redX - X) <= 28 && Math.abs(redY - Y) <= 28) || (Math.abs(pinkX - X) <= 28 && Math.abs(pinkY - Y) <= 28) || (Math.abs(cyanX - X) <= 28 && Math.abs(cyanY - Y) <= 28) || (Math.abs(orangeX - X) <= 28 && Math.abs(orangeY - Y) <= 28))         
+				{
+					gameOver = true;
+					if(!shown)
+					{
+						JOptionPane.showMessageDialog(null, "GAME OVER!");
+				    	shown = true;
+				    	PacManLabel.setVisible(false);
+				    	red.setVisible(false);
+				    	pink.setVisible(false);
+				    	cyan.setVisible(false);
+				    	orange.setVisible(false);
+				    	for(int i = 0; i < borders.size(); i++)
+				    	{
+				    		borders.get(i).border.setVisible(false);
+				    	}
+				    	for(int i = 0; i < pellets.size(); i++)
+				    	{
+				    		pellets.get(i).border.setVisible(false);
+				    	}
+				    	nameLabel.setVisible(true);
+				    	nameLabel.setText("Game Over");
+				    	nameLabel.setFont(new Font("", Font.PLAIN, 50)); 
+				    	nameLabel.setBounds(380,100,300,100);
+						score.setBounds(410, 400, 350, 100);
+						score.setFont(new Font("", Font.BOLD, 40));
+				    	pacman_right.setVisible(false);
+						pacman_left.setVisible(false);
+						pacman_up.setVisible(false);
+						pacman_down.setVisible(false);
+						topBorder1.setVisible(false);
+						bottomBorder1.setVisible(false);
+						sideBorder1.setVisible(false);
+						sideBorder2.setVisible(false);
+					}
+				}
+		    	else
+		    	{
+		    		System.out.println(X);
+		    		if(X <= 16)
+		    		{
+		    			System.out.println("Success");
+		    			for(int i = 0; i < 25; i++)
+			    		{
+			    				if((Math.abs(pellets.get(i).getX() - X) <= 30 && Math.abs(pellets.get(i).getY() - Y) <= 30))
+			    				{
+			    					pellets.get(i).border.setVisible(false);
+						    		score1++;
+						        	score.setText("Score = " + score1);
+			    				}
+			    		}
+		    		}
+		    		else if(X <= 50)
+		    		{
+		    			for(int i = 25; i < 50; i++)
+			    		{
+			    				if((Math.abs(pellets.get(i).getX() - X) <= 30 && Math.abs(pellets.get(i).getY() - Y) <= 30))
+			    				{
+			    					pellets.get(i).border.setVisible(false);
+						    		score1++;
+						        	score.setText("Score = " + score1);
+						        	break;
+			    				}
+			    		}
+		    		}
+			        redX = move(redX); 						if(redX >= 948){ redX = 948;} if(redX <= 10){ redX = 10;} 
+			        redY = move(redY); 						if(redY >= 611){ redY = 611;} if(redY <= 10){ redY = 10;} 
+					red.setBounds(redX, redY, 30, 42);
+					pinkX = move(pinkX); 					if(pinkX >= 948){ pinkX = 948;} if(pinkX <= 10){ pinkX = 10;} 
+					pinkY = move(pinkY);					if(pinkY >= 611){ pinkY = 611;} if(pinkY <= 10){ pinkY = 10;} 
+			        pink.setBounds(pinkX, pinkY, 30, 42);
+			        cyanX = move(cyanX); 					if(cyanX >= 948){ cyanX = 948;} if(cyanX <= 10){ cyanX = 10;} 
+			        cyanY = move(cyanY);					if(cyanY >= 611){ cyanY = 611;} if(cyanY <= 10){ cyanY = 10;} 
+			        cyan.setBounds(cyanX, cyanY, 30, 42);
+			        orangeX = move(orangeX); 		if(orangeX >= 948){ orangeX = 948;} if(orangeX <= 10){ orangeX = 10;} 
+			        orangeY = move(orangeY);		if(orangeY >= 611){ orangeY = 611;} if(orangeY <= 10){ orangeY = 10;} 
+			        orange.setBounds(orangeX, orangeY, 30, 42);
+		    	}
 		    };  
 		}; 
 	    t.scheduleAtFixedRate( tt, 50, 100);
@@ -110,15 +176,30 @@ public class Enemies extends Main
 				}
 		    	else
 		    	{
-//		    		for(int i = 0; i < pellets.size(); i++)
-//			    	{
-//						if((Math.abs(X - pellets.get(i).getX()) <= 100) && (Math.abs(Y - pellets.get(i).getY()) >= 30))         
-//						{
-//			    		pellets.get(i).border.setVisible(false);
-//			    		score1++;
-//			        	score.setText("Score = " + score1);
-//						}
-//			    	}
+		    		if(X <= 10)
+		    		{
+		    			for(int i = 0; i < 25; i++)
+			    		{
+			    				if((Math.abs(pellets.get(i).getX() - X) <= 30 && Math.abs(pellets.get(i).getY() - Y) <= 30))
+			    				{
+			    					pellets.get(i).border.setVisible(false);
+						    		score1++;
+						        	score.setText("Score = " + score1);
+			    				}
+			    		}
+		    		}
+		    		else if(X <= 50)
+		    		{
+		    			for(int i = 25; i < 50; i++)
+			    		{
+			    				if((Math.abs(pellets.get(i).getX() - X) <= 30 && Math.abs(pellets.get(i).getY() - Y) <= 30))
+			    				{
+			    					pellets.get(i).border.setVisible(false);
+						    		score1++;
+						        	score.setText("Score = " + score1);
+			    				}
+			    		}
+		    		}
 		    		redX = hardMoveX(redX); 			
 		    		redY = hardMoveY(redY); 
 		    		red.setBounds(redX, redY, 30, 42);
